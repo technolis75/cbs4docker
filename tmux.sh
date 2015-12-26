7b4513916a0c3d82e9a2562413c0fcc2cdb8c8d8
technolis75 <harry.bonneto@technolis.fr>
2015-12-26 17:18:58 +0100

    Update tmux.sh

--- tmux.sh
+++ tmux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-tmux new-session -d '/obsr/bin/startup.sh'
+tmux new-session -d '/usr/local/cbs/bin/startup.sh'
 tmux split-window -v 'sleep 5 && tail -F logs/obs_context*'
 tmux attach-session -d
